b5d6cea57892539a19c10045166b359a2da33623
rupomsoft <rupomplaystore@gmail.com>
2021-07-28 09:22:09 +0600

    psuhasd
    asd

--- my-app/client/src/App.js
+++ my-app/client/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     render() {
         return (
             <div>
-
+                    <h1>Hello</h1>
             </div>
         );
     }
--- my-app/server.js
+++ my-app/server.js
@@ -6,6 +6,11 @@ const {Server}= require('socket.io');
 const io=new Server(expressServer);
 const port = process.env.PORT || 5000; //Line 3
 
+
+app.use(express.static('client/build'));
+const path = require('path');app.get('*', (req, res) => {res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));});
+
+
 app.get('/express_backend',function (req,res){
     res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT....' });
 });
